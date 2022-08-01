0bace60e3ee44e3bc84a9484e913b790e7f876b2
PC <PC@DESKTOP-R8SP3AG>
2022-08-01 15:42:31 -0700

    First Commit

---
+++ Day-1_Git and GitHub/src/com/capgemini/github/EclipseWithGithub.java
@@ -0,0 +1,12 @@
+package com.capgemini.github;
+//demo for to connect eclipse with GitHub
+public class EclipseWithGithub {
+
+	public static void main(String[] args) {
+		//print the line
+		System.out.println("Welcome to Capgemini C2TC Program");
+		
+
+	}
+
+}
