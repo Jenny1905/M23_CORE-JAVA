019fc785ccea0c5a07f663fc1a2e6c0dceba4397
PC <PC@DESKTOP-R8SP3AG>
2022-08-02 15:44:28 -0700

    Second Commit

--- Day-2_Introduction to Java/src/com/capgemini/introduction/ByteProgram.java
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/ByteProgram.java
@@ -1,9 +1,13 @@
 package com.capgemini.introduction;
 
-public class ByteProgram {
 
+//program for byte data-type
+public class ByteProgram {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		byte a=127;
+		//byte a=129 
+		//it will give you an error because of byte range
+		System.out.println(a);
 
 	}
 
