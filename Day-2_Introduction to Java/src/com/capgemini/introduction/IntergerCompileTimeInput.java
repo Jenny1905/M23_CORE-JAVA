930c5205dc3412e572b0aca6b60581f7a5b2e24d
PC <PC@DESKTOP-R8SP3AG>
2022-08-02 16:19:31 -0700

    Second Commit

---
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/IntergerCompileTimeInput.java
@@ -0,0 +1,17 @@
+package com.capgemini.introduction;
+//program on Integer data-type=compile time input
+public class IntergerCompileTimeInput {
+
+	public static void main(String[] args) {
+		
+		//variable initialization
+		int a=145;
+		System.out.println(a);
+		//"println" is used to print the output in the next line
+		//"print" is used to print the output in the same line
+		System.out.println("Hey Guys!");
+		System.out.println("Hey folks!");
+
+	}
+
+}
---
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/StringProgram.java
@@ -0,0 +1,17 @@
+package com.capgemini.introduction;
+//program on string input
+import java.util.Scanner;
+
+public class StringProgram {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		//next() will take only first word and
+		//nextline() will take entire line
+		//String str=sc.next();
+		String str=sc.nextLine();
+		System.out.println(str);
+
+	}
+
+}
