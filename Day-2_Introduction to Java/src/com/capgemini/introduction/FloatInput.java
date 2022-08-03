af4c1f0f1621c714eb986481a4f460ef81405dd2
PC <PC@DESKTOP-R8SP3AG>
2022-08-03 15:25:33 -0700

    Second Commit

---
+++ Day-2_Introduction to Java/src/com/capgemini/introduction/FloatInput.java
@@ -0,0 +1,17 @@
+package com.capgemini.introduction;
+
+import java.util.Scanner;
+
+public class FloatInput {
+
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+		float f=12.5f;
+		//System.out.println("Enter the value: ");
+		//float f=s.nextFloat();
+		System.out.println(f);
+		s.close();
+
+	}
+
+}
