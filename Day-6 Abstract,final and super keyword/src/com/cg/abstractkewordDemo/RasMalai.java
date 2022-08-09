ad095a781603ab86dbc180f42f169ef90c8589e6
PC <PC@DESKTOP-R8SP3AG>
2022-08-09 14:53:13 -0700

    First Commit

---
+++ Day-6 Abstract,final and super keyword/src/com/cg/abstractkewordDemo/RasMalai.java
@@ -0,0 +1,13 @@
+package com.cg.abstractkewordDemo;
+//abstract class
+public abstract class RasMalai
+{
+	//constructor
+	public RasMalai()
+	{
+		System.out.println("Favourite Sweet");
+	}
+	//abstract method
+	abstract void print();
+
+}
