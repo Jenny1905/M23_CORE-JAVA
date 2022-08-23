31c402a596306195d0ba69cf15fd5a6621398cab
PC <PC@DESKTOP-R8SP3AG>
2022-08-23 16:21:59 -0700

    Second Commit

--- Day-16_Generics/src/com/cg/generics/Executor.java
+++ Day-16_Generics/src/com/cg/generics/Executor.java
@@ -1,29 +1,27 @@
 package com.cg.generics;
 
+
 public class Executor {
 
 	public static void main(String[] args) {
-		//create a sender to send a string message
-		Sender<String>obj=new Sender<>();
-		obj.setMessage("Generic Class Demo");
-		obj.sendMessage();
 		
-		System.out.println();
+		//create a sender to send string message
+		Sender<String> stringSender = new Sender<String>();
+		stringSender.setMessage("Hello from Java!");
+		stringSender.sendMessage();
 		
-		//create an email object to be send using sender
-		Email em=new Email();
-		em.setFrom("skumbhalkar@tnsif.org");
-		em.setTo("shraddhakharnare@gmail.com");
-		em.setSubject("Java Full Stack");
-		em.setBody("About Training and placement");
-		System.out.println(em.getBody());
+		//create an email object to be sent using sender
+		Email myEmail = new Email();
+		myEmail.setFrom("test@igate.com");
+		myEmail.setTo("admin@igate.com");
+		myEmail.setSubject("Need to block cafeteria for personal event");
+		myEmail.setBody("To celebrate IGATE birthday!");
+		
 		//create a sender to send this email
-		Sender<Email>obj1=new Sender<Email>();
-		obj1.setMessage(em);
+		Sender<Email> emailSender = new Sender<Email>();
+		emailSender.setMessage(myEmail);
+		emailSender.sendMessage();
 		
-		obj1.sendMessage();
 		
-
 	}
-
 }
